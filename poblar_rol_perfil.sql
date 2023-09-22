a0d60ce34cc754fa22c97884e2d67498d586bdaa
AracelyC1 <aracely.castro@usach.cl>
2023-09-21 21:41:25 -0300

    Create poblar_rol_perfil.sql
    Poblar las tablas rol y perfil

---
+++ poblar_rol_perfil.sql
@@ -0,0 +1,20 @@
+-- poblar tabla ROL
+insert into rol (rol) values ('coordinador');
+insert into rol (rol) values ('voluntario');
+
+
+-- poblar tabla PERFIL
+insert into perfil (foto_perfil, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, descripcion_perfil, genero, fecha_nacimiento)
+values('imagen 1', 'Ana', 'María', 'Rodríguez', 'López', 'Estudiante de Arquitectura', 'Femenino', '1995-03-15');
+
+insert into perfil (foto_perfil, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, descripcion_perfil, genero, fecha_nacimiento)
+values ('imagen 2', 'Juan', 'Carlos', 'Pérez', 'García', 'Ingeniero de Software', 'Masculino', '1988-07-22');
+
+insert into perfil (foto_perfil, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, descripcion_perfil, genero, fecha_nacimiento)
+values('imagen 3', 'Laura', 'Andrea', 'Gómez', 'Martínez', 'Médica pediatra', 'Femenino', '1990-05-10');
+
+insert into perfil (foto_perfil, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, descripcion_perfil, genero, fecha_nacimiento)
+values('imagen 4', 'Alejandro', 'Javier', 'González', 'Ramírez', 'Abogado de derechos civiles', 'Masculino', '1985-11-30');
+
+insert into perfil (foto_perfil, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, descripcion_perfil, genero, fecha_nacimiento)
+values('imagen 5', 'María', 'Isabel', 'López', 'Fernández', 'Estudiante de Psicología', 'Femenino', '1997-02-18');
