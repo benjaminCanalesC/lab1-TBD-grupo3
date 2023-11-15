f12674e309df6e945593ba00bf67c39fd3a764d2
AracelyC1 <aracely.castro@usach.cl>
2023-11-15 13:28:17 -0300

    Update direcciones_voluntarios.sql
    Agregado las emergencias con postgis

--- direcciones_voluntarios.sql
+++ direcciones_voluntarios.sql
@@ -1,16 +1,24 @@
--- eliminar tabla de direcciones
-DROP TABLE addresses;
+-- eliminar tabla de direcciones de voluntarios y emergencia
+DROP TABLE volunteer_addresses;
+DROP TABLE emergency_addresses;
 
 -- crear tabla de direcciones
-CREATE TABLE addresses (
+CREATE TABLE volunteer_addresses (
   id_address SERIAL PRIMARY KEY,
   address VARCHAR(255) NOT NULL,
   latitude FLOAT NOT NULL,
   longitude FLOAT NOT NULL
 );
 
+CREATE TABLE emergency_addresses (
+  id_address_e SERIAL PRIMARY KEY, -- en este caso emergency tiene las direcciones 
+  address VARCHAR(255) NOT NULL,
+  longitude FLOAT NOT NULL,
+  latitude FLOAT NOT NULL
+);
+
 -- poblar la tabla de direcciones
-INSERT INTO addresses (id_address, address, latitude, longitude)
+INSERT INTO volunteer_addresses (id_address, address, latitude, longitude)
 VALUES
   (1, 'Caupolicán 1027, Concepción, Bío Bío', -73.0547538041757, -36.821393959657435),
   (2,'Avda. Fco de Aguirre 170, 1710156 La Serena, Coquimbo', -71.25461346356609, -29.90545603158812),
@@ -37,15 +45,39 @@ VALUES
   (23 ,'Caupolicán 660, Providencia, Región Metropolitana', -70.62165556569133, -33.44738511181257),
   (24 ,'Pje. 11 de Septiembre 230, Mejillones, Antofagasta', -70.44292641837411, -23.099618075044457);
   
+  INSERT INTO emergency_addresses (id_address_e, address, longitude, latitude) VALUES
+  (1, 'Lautaro, Araucanía', -38.57434144039071, -72.4756987329032),
+  (2, 'Viña del Mar, Valparaíso', -32.993457378637764, -71.53361941561872),
+  (3, 'Pucón, Araucanía', -39.41955618986563, -71.93942618853083),
+  (4, 'Lago Ranco, Los Ríos', -40.57747844827906, -72.10983024396644),
+  (5, 'Mall Zofri - Sector antiguo, modulo 130, Iquique, Tarapacá', -20.22959943149161, -70.13553539158428),
+  (6, 'Sucre 1641, 1340517 Tocopilla, Antofagasta', -22.093843895975603, -70.20005600801197),
+  (7, 'Padre Felipe Gómez de Vidaurre, 8330369 Santiago, Región Metropolitana', -33.447969674529496, -70.65607952327353),
+  (8, 'Candelaria Pérez 26-66, Illapel, Coquimbo', -31.631798107170333, -71.17459210501087),
+  (9, 'Av. Angamos 290-194, Antofagasta', -23.67586252273854, -70.40806255684329),
+  (10, 'Constitución, Maule', -35.33074968305388, -72.43287959887027),
+  (11, 'Villa Santa Lucía, Chaitén, Los Lagos', -43.4106060512741, -72.36752613467937),
+  (12, 'Los Angeles, Bío Bío', -37.42699254228149, -72.3287247972075);
+  
+  
   -- agregar si es que no se tiene la extensión de postgis
   -- CREATE EXTENSION postgis;
   
-  select * from addresses;
+  select * from volunteer_addresses;
+  select * from emergency_addresses;
   
   -- agregar la columna para colocar la información georeferenciada
-  ALTER TABLE addresses ADD COLUMN geom geometry(Point, 4326);
+  ALTER TABLE volunteer_addresses ADD COLUMN geom geometry(Point, 4326);
+  ALTER TABLE emergency_addresses ADD COLUMN geom geometry(Point, 4326);
   
   -- poblar la columna de geom
-  UPDATE addresses SET geom = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+  UPDATE volunteer_addresses SET geom = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+  UPDATE emergency_addresses SET geom = ST_SetSRID(ST_MakePoint(latitude, longitude), 4326);
+  
+  
+  
+  
+  
+  
   
   
